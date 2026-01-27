72f460ee852ace39d2024b78488448fbb9b60628
lshee9008 <92621233+lshee9008@users.noreply.github.com>
2026-01-27 13:07:30 +0900

    baekjoon_10430

---
+++ baekjoon/python/baekjoon_10430/main.py
@@ -0,0 +1,10 @@
+def the_rest():
+    A, B, C = map(int, input().split())
+    print((A + B) % C)
+    print(((A % C) + (B % C)) % C)
+    print((A * B) % C)
+    print(((A % C) * (B % C)) % C)
+
+
+if __name__ == '__main__':
+    the_rest()
