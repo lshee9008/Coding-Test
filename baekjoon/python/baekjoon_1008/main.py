8e47222ca9041ac2fc7148efcc953a4b76805156
lshee9008 <92621233+lshee9008@users.noreply.github.com>
2026-01-27 12:39:34 +0900

    baekjoon_1008 & baekjoon_10869

---
+++ baekjoon/python/baekjoon_1008/main.py
@@ -0,0 +1,7 @@
+def print_A_div_B():
+    A, B = input().split()
+    print(int(A) / int(B))
+
+
+if __name__ == '__main__':
+    print_A_div_B()
---
+++ baekjoon/python/baekjoon_10869/main.py
@@ -0,0 +1,11 @@
+def calculation():
+    lst = list(map(int, input().split()))
+
+    print(lst[0] + lst[1])
+    print(lst[0] - lst[1])
+    print(lst[0] * lst[1])
+    print(lst[0] // lst[1])
+    print(lst[0] % lst[1])
+
+if __name__ == '__main__':
+    calculation()
